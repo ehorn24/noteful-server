83cd0fbbd799dc29cb93369411806d90e8949090
Ella Horn <ellalouisahorn@yahoo.com>
2020-03-03 16:07:19 -0500

    comma

--- migrations/004.do.seed_notes.sql
+++ migrations/004.do.seed_notes.sql
@@ -1,4 +1,4 @@
-INSERT INTO "public"."noteful_notes"("notename", "folderid", "content",, "modified" ) VALUES('Praveen', 1, 'Praveen is awesome!', now());
+INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified" ) VALUES('Praveen', 1, 'Praveen is awesome!', now());
 INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('Ella', 2, 'Ella is fantastic!', now());
 INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('Extra Important Stuff', 3, 'This is necessary information', now());
 INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('John', 2, 'John is great!', now());
