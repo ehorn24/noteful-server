cb69c25e02117d992c859606bffcaaba2cdc7fff
Ella Horn <ellalouisahorn@yahoo.com>
2020-03-03 15:28:26 -0500

    database url defined twice, removed one

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 
-const DATABASE_URL = process.env.DATABASE_URL;
-
 const db = knex({
   client: "pg",
   connection: DATABASE_URL
