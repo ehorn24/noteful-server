4cdddd81091976e481dc4c000b5e5b188bb3f5d4
Ella Horn <ellalouisahorn@yahoo.com>
2020-03-03 16:04:55 -0500

    add dates

--- migrations/004.do.seed_notes.sql
+++ migrations/004.do.seed_notes.sql
@@ -1,5 +1,5 @@
-INSERT INTO "public"."noteful_notes"("notename", "folderid", "content") VALUES('Praveen', 1, 'Praveen is awesome!');
-INSERT INTO "public"."noteful_notes"("notename", "folderid", "content") VALUES('Ella', 2, 'Ella is fantastic!');
-INSERT INTO "public"."noteful_notes"("notename", "folderid", "content") VALUES('Extra Important Stuff', 3, 'This is necessary information');
-INSERT INTO "public"."noteful_notes"("notename", "folderid", "content") VALUES('John', 2, 'John is great!');
-INSERT INTO "public"."noteful_notes"("notename", "folderid", "content") VALUES('Cookies', 5, 'my cookie recipe');
+INSERT INTO "public"."noteful_notes"("notename", "folderid", "content",, "modified" ) VALUES('Praveen', 1, 'Praveen is awesome!', now());
+INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('Ella', 2, 'Ella is fantastic!', now());
+INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('Extra Important Stuff', 3, 'This is necessary information', now());
+INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('John', 2, 'John is great!', now());
+INSERT INTO "public"."noteful_notes"("notename", "folderid", "content", "modified") VALUES('Cookies', 5, 'my cookie recipe', now());
